b3f87b88705b090b0f71f5de568d8453ac951207
Felipe Simmi <simmifelipe1992@gmail.com>
2020-08-04 21:48:20 -0300

    Criada aplicação web do nlw2 (faltando formulario)

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars0.githubusercontent.com/u/15945104?s=460&u=a95b9c60ddd504e259e20aff13512adbc4d024ca&v=4"
+          alt="Felipe Simmi"
+        />
+
+        <div>
+          <strong>Felipe Simmi</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /> <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
